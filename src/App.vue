1ae51a9b5a21a74020b5f9b68ecf3617091db497
kalwalt <github@kalwaltart.it>
2021-03-31 17:11:14 +0200

    first commit - adding a basic structure

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/Visual_AR_Poetry.png">
+  <Header msg="Visual AR Poetry"/>
+  <Intro title="A generative app for visual poetry" text="Visualize every day a different visual poetry"/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Header from './components/Header.vue';
+import Intro from './components/Intro.vue';
+
+@Options({
+  components: {
+    Header,
+    Intro,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+        <p>{{ text }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    title: String,
+    text: String
+  }
+})
+export default class Intro extends Vue {
+  title!: string;
+  text!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 40px 0 0;
+}
+</style>
