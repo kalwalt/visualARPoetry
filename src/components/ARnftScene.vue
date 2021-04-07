c3fe48a42ea52c1d5d95e4cc423425a035ce6aeb
kalwalt <github@kalwaltart.it>
2021-04-07 22:48:57 +0200

    testing the new ARnft options

--- src/components/ARnftScene.vue
+++ src/components/ARnftScene.vue
@@ -1,5 +1,12 @@
 <template>
-    <div></div>
+    <div id="loading">
+      <img src="arNFT-logo.gif" alt="arNFT.js logo">
+      <span class="loading-text">Loading, please wait...</span>
+    </div>
+    <div id="app">
+      <canvas id="canvas" width="640" height="480"></canvas>
+      <video id="video" muted playsinline autoplay></video>
+    </div>
 </template>
 
 <script>
@@ -24,7 +31,7 @@ export default {
     methods: {
         init() {
 
-            ARnft.init(640, 480, "../DataNFT/pinball", '../config.json', true)
+            ARnft.init(640, 480, "../DataNFT/pinball", '../config.json', false)
             .then((nft) => {
                 let mat = new THREE.MeshLambertMaterial({color: 0xff0000});
                 let boxGeom = new THREE.BoxGeometry(1,1,1);
@@ -44,3 +51,59 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+#app {
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+}
+
+#video {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  display: block;
+  width: 100% !important;
+  height: 100% !important;
+  object-fit: cover;
+}
+
+#canvas {
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 100;
+
+  display: block;
+  width: 100% !important;
+  height: 100% !important;
+  object-fit: cover;
+}
+
+#loading {
+  width: 100%;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.45);
+  z-index: 9999999;
+}
+
+#loading img {
+    height: 5em;
+}
+
+#loading span{
+  color: black;
+  font-weight: bold;
+}
+</style>
