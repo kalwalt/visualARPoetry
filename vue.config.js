f7786ce8f9866f483ce85df2007be1e71b9f9251
kalwalt <github@kalwaltart.it>
2021-04-07 17:27:17 +0200

    addin id "visual" and chunks

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,14 @@ module.exports = {
         template: 'public/index.html',
         // output as dist/index.html
         filename: 'index.html',
+        chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
       },
       visual: {
           entry: 'src/pages/visual.ts',
           template: 'public/visual.html',
           filename: 'visual.html',
-          title: 'Visual page'
+          title: 'Visual page',
+          chunks: [ 'chunk-vendors', 'chunk-common', 'visual' ]
       }
     }
   }
