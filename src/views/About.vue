33c6ca91398f7ade424cc9909242a434eb741b20
kalwalt <github@kalwaltart.it>
2021-04-12 16:43:46 +0200

    LayoutAbout.vue component for the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Layout/>
 </template>
+
+<script>
+import Layout from '@/components/LayoutAbout.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Layout
+  }
+
+}
+</script>
