a0740d93d103eb80309a71f1ad4ccac033e4bc89
kalwalt <github@kalwaltart.it>
2022-01-31 18:54:47 +0100

    testing new Masonry gallery

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -11,6 +11,7 @@
     intro="Visualize every day a different visual poetry, made with text of poems, geometries and glitch noises."
     text="Download the picture and print it! You will track with your camera Phone to display a generated image!"
     />
+    <Masonry/>
   </div>
   <div class="section has-text-centered">Made by <a href="https://github.com/kalwalt">@kalwalt</a></div>
 </template>
@@ -19,13 +20,15 @@
 // @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Intro from '@/components/Intro.vue'
+import Masonry from '@/components/Masonry.vue'
 
 
 export default {
   name: 'Home',
   components: {
     Header,
-    Intro
+    Intro,
+    Masonry
   }
 }
 </script>
---
+++ src/components/Masonry.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="section">
+    <div
+      class="columns"
+      data-masonry='{"percentPosition": true }'
+    >
+      <div class="column" v-for="(card, index) in collection" :key="index">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                :src="card.urlToImage"
+                alt="image link lost"
+                data-bs-toggle="collapse"
+                :data-bs-target="'#collapseImage' + index"
+              />
+            </figure>
+          </div>
+
+          <h5>
+            <a :href="card.url" target="_blank">{{ card.title }}</a>
+          </h5>
+          <div class="collapse" :id="'collapseImage' + index">
+            <div class="card-content">
+              <span v-html="card.description"></span>
+            </div>
+          </div>
+          <div class="card-footer">
+            {{ card.publishedAt }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Masonry from "masonry-layout";
+
+export default {
+  name: Masonry,
+  data() {
+    return {
+      collection: [
+        {
+          urlToImage:
+            "https://raw.githubusercontent.com/kalwalt/visualARPoetry-backend/main/src/imgs/fishes.jpg",
+          title: "title1",
+          description: "short description",
+        },
+        {
+             urlToImage:
+            "https://raw.githubusercontent.com/kalwalt/visualARPoetry-backend/main/src/imgs/fishes.jpg",
+          title: "title2",
+          description:
+            "Lorem ipsum dolor sit amet...",
+        },
+        {
+          title: "title3",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae orci sed urna ultricies aliquam. Nulla a massa neque. Nunc commodo eros ex, at faucibus lacus molestie non.",
+        },
+        {
+          urlToImage:
+            "https://raw.githubusercontent.com/kalwalt/visualARPoetry-backend/main/src/imgs/fishes.jpg",
+          title: "",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae orci sed urna ultricies aliquam. Nulla a massa neque. Nunc commodo eros ex, at faucibus lacus molestie non.",
+        },
+        {
+          title: "",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae orci sed urna ultricies aliquam. Nulla a massa neque. Nunc commodo eros ex, at faucibus lacus molestie non.",
+        },
+        {
+          title: "",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae orci sed urna ultricies aliquam. Nulla a massa neque. Nunc commodo eros ex, at faucibus lacus molestie non.",
+        },
+        {
+          title: "",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae orci sed urna ultricies aliquam. Nulla a massa neque. Nunc commodo eros ex, at faucibus lacus molestie non.",
+        },
+        {
+          title: "",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae orci sed urna ultricies aliquam. Nulla a massa neque. Nunc commodo eros ex, at faucibus lacus molestie non.",
+        },
+      ],
+    };
+  },
+  mounted() {
+    // initialize masonry
+    var row = document.querySelector("[data-masonry]");
+    new Masonry(row, {
+      // options
+      percentPosition: true,
+    });
+  },
+};
+</script>
